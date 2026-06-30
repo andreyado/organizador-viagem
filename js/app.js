f1c72de1f2e5b3df60f74caf2906773b7a32a217
andreyado <andreyado@outlook.com>
2026-06-30 19:24:47 -0300

    Integrate Storage for user plan retrieval
    Added Storage module to handle user plans on login success.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import Auth from "./auth.js";
 import LoopSystem from "./loopsystem.js";
 import Router from "./router.js";
+import Storage from "./storage.js";
 
 const App = {
 
@@ -8,6 +9,15 @@ const App = {
         Router.init();
         LoopSystem.init();
         Auth.bindEvents();
+
+        Auth.onLoginSuccess = async (user) => {
+            const plan = await Storage.loadPlan(user.email);
+            if(plan){
+                LoopSystem.goDashboard(plan);
+            } else {
+                Router.show("home");
+            }
+        };
 
         Router.show("splash");
 
